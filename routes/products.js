2730507a3c152f6758cc67dbfef9d9dab53e5fbb
Olga Shiryaeva <oshiryaeva@protonmail.com>
2021-12-01 20:31:55 -0600

    Add products endpoint, router, data

---
+++ routes/products.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+const fs = require('fs');
+const path = require('path');
+const { dbQuery } = require("../lib/db-query");
+const catchError = require("../lib/catch-error");
+
+/* GET users listing. */
+router.get('/', catchError(async (req, res, next) => {
+    let result = await getProducts();
+    res.json(result.rows);
+}));
+
+  // TODO extract these later
+async function getProducts() {
+  const SQL = "SELECT * FROM products";
+
+  let result = await dbQuery(SQL);
+  return result;
+};
+
+module.exports = router;
