781bf88ce325478372bc1bc00adeb372a8b673ef
Olga Shiryaeva <oshiryaeva@protonmail.com>
2021-12-02 15:20:14 -0600

    Add routers for addresses, books, companies, movies, products, users

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ const logger = require('morgan');
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const productsRouter = require('./routes/products');
+const companiesRouter = require('./routes/companies');
+const addressesRouter = require('./routes/addresses');
+const booksRouter = require('./routes/books');
+const moviesRouter = require('./routes/movies');
 
 const app = express();
 
@@ -23,6 +27,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/products', productsRouter);
+app.use('/companies', companiesRouter);
+app.use('/addresses', addressesRouter);
+app.use('/books', booksRouter);
+app.use('/movies', moviesRouter);
+
+app.get('/docs', (re, res, next) => {
+  res.sendFile('index.html', { root: path.join(__dirname, '/public/doc') });
+})
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
