873b03ed381de950d6e96a378b8caf36e854aaef
Olga Shiryaeva <oshiryaeva@protonmail.com>
2021-12-05 14:35:59 -0600

    Add rate limiter

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
+const requestIP = require('request-ip');
+const nodeCache = require('node-cache');
+const isIp = require('is-ip');
+
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const productsRouter = require('./routes/products');
@@ -12,11 +16,56 @@ const addressesRouter = require('./routes/addresses');
 const booksRouter = require('./routes/books');
 const moviesRouter = require('./routes/movies');
 
+const {
+  TIME_FRAME_IN_S,
+  TIME_FRAME_IN_MS,
+  MS_TO_S,
+  RPS_LIMIT
+} = require('./utils/constants/rate-limiting-const');
+
+const IPCache = new nodeCache({ stdTTL: TIME_FRAME_IN_S, deleteOnExpire: false, checkperiod: TIME_FRAME_IN_S });
+IPCache.on('expired', (key, value) => {
+  if (new Date() - value[value.length - 1] > TIME_FRAME_IN_MS) {
+      IPCache.del(key);
+  } else {
+    const updatedValue = value.filter(function (element) {
+        return new Date() - element < TIME_FRAME_IN_MS;
+    });
+    IPCache.set(key, updatedValue, TIME_FRAME_IN_S - (new Date() - updatedValue[0]) * MS_TO_S);
+}
+});
+
 const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
+const ipMiddleware = async function (req, res, next) {
+  let clientIP = requestIP.getClientIp(req);
+  if (isIp.v6(clientIP)) {
+      clientIP = clientIP.split(':').splice(0, 4).join(':') + '::/64';
+  }
+
+  updateCache(clientIP);
+  const IPArray = IPCache.get(clientIP);
+  if (IPArray.length > 1) {
+      const rps = IPArray.length / ((IPArray[IPArray.length - 1] - IPArray[0]) * MS_TO_S);
+      if (rps > RPS_LIMIT) {
+          console.log('You are hitting limit', clientIP);
+      }
+  }
+  next();
+};
+
+const updateCache = (ip) => {
+  let IPArray = IPCache.get(ip) || [];
+  IPArray.push(new Date());
+  IPCache.set(ip, IPArray, (IPCache.getTtl(ip) - Date.now()) * MS_TO_S || TIME_FRAME_IN_S);
+};
+
+
+app.use(ipMiddleware);
 
 app.use(logger('dev'));
 app.use(express.json());
