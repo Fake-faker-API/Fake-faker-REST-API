3bd1a583872608ad45e74c3dd59695f5142929cb
Olga Shiryaeva <oshiryaeva@protonmail.com>
2021-12-05 14:35:31 -0600

    Constants for the rate limiter

---
+++ utils/constants/rate-limiting-const.js
@@ -0,0 +1,11 @@
+const TIME_FRAME_IN_S = 10;
+const TIME_FRAME_IN_MS = TIME_FRAME_IN_S * 1000;
+const MS_TO_S = 1 / 1000;
+const RPS_LIMIT = 2;
+
+module.exports = {
+  TIME_FRAME_IN_S,
+  TIME_FRAME_IN_MS,
+  MS_TO_S,
+  RPS_LIMIT
+}
